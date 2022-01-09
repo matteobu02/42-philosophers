6087fb1c97ff8f0bf7da2b35b0fbe1f16c15a5d1
Matteo Bucci <mbucci@fu-r5-p5.s19.be>
2022-01-09 11:53:43 +0100

    yeah

--- srcs/actions.c
+++ srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 11:38:17 by mbucci            #+#    #+#             */
-/*   Updated: 2022/01/09 11:43:44 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/01/09 11:53:16 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 void	philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(philo.rfork);
+	print_message();		// has taken a fork
 	pthread_mutex_lock(philo.lfork);
-	print_message()
+	print_message();		// has taken a fork
+	print_message();		// is eating
 	ft_usleep(philo.env->time_eat);
 	pthread_mutex_unlock(philo.rfork);
 	pthread_mutex_unlock(philo.lfork);
